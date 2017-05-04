5cfc9a9ebbab6c625a657d0012b9041f59210f60
singy pig-thingy <8mjoyner@stpatrick.org>
2017-05-04 13:59:58 -0500

    Kek-tastic updarte.
    Ynot. Kek-tastic.

--- string.js
+++ string.js
@@ -6,10 +6,13 @@ function PasswordMessage(m){
 }
 function inRange(char,min,max){
     let unicode = char.charCodeAt(0);
-
-
+    if(character<min || character>max){
+        return false;
+    }
+    else{
+        return false;
+    }
 }
-
 exports.checkLength = function(str){
     let valid = (str.length >= 8 && str.length <= 20);
     try{
@@ -28,20 +31,59 @@ exports.checkLength = function(str){
         return valid;
     }
 }
-
-
 exports.containsUpper =function(str){
-
+    let hasUpper = FALSE;
+    try{
+        for(let x=0;x<str.length;x++){
+            if(inRange(str>=65 && <=90)=true){
+            hasUpper = true;
+            throw new PasswordMessage("Password has an uppercase letter.");
+        }
+    }
+        if(hasUpper = FALSE){
+            throw new PasswordMessage("Password does not have an uppercase letter.");
+        }
+    }
+        catch(e){
+            console.log(e.name+": "+e.message);
+            return hasUpper;
+    }
 }
-
-
 exports.containsLower =function(str){
-
+    let hasLower = FALSE;
+    try{
+        for(let x=0;x<str.length;x++){
+            if(inRange(str>=97 && <=122)=true){
+            hasLower = true;
+            throw new PasswordMessage("Password has an lowercase letter.");
+        }
+    }
+        if(hasLower = FALSE){
+            throw new PasswordMessage("Password does not have an lowercase letter.");
+        }
+    }
+        catch(e){
+            console.log(e.name+": "+e.message);
+            return hasLower;
+    }
 }
-
-
 exports.containsNumerical =function(str){
-
+    let hasNumerical = FALSE;
+    try{
+        for(let x=0;x<str.length;x++){
+            if(inRange(str>=48 && <=57)=true){
+            hasNumerical = true;
+            throw new PasswordMessage("Password has an numerical letter.");
+        }
+    }
+        if(hasNumerical = FALSE){
+            throw new PasswordMessage("Password does not have an numerical letter.");
+        }
+    }
+        catch(e){
+            console.log(e.name+": "+e.message);
+            return hasNumerical;
+    }
 }
 
 
